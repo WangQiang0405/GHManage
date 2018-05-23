aea99f8e126afe53eb88c7f74179e6a4cbbcab05
WangQiang <wang_q0405@163.com>
2018-05-23 14:58:02 +0800

    ghDetail update 2018/05/18 by wang-q

--- WebContent/js/manage.js
+++ WebContent/js/manage.js
@@ -491,5 +491,5 @@ function windowpopup(id){
     // name="1";
     var top = (screen.height - 300) / 2;
     var left = (screen.width - 1500) / 2;
-    window.open ( "Ghdetail?id="+id , "_blank" , 'height=300,width=1500,top='+top+',left='+left+',toolbar=no,menubar=no,scrollbars=yes, resizable=yes,location=no, status=no');
+    window.open ("Ghdetail?id="+id,"_blank",'height=300,width=1500,top='+top+',left='+left+',toolbar=no,menubar=no,scrollbars=yes,resizable=yes,location=no,status=no');
 }
